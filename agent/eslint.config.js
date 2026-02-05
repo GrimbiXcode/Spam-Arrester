7a2b3498bc165af06f882d96977a8f9e2919b981
David Grimbichler <grimbixcode@gmail.com>
2026-02-05 21:35:47 +0100

    fix: add missing ESLint config for agent

---
+++ agent/eslint.config.js
@@ -0,0 +1,31 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**', '**/*.test.ts', '**/__tests__/**'],
+  },
+];
